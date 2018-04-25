976b7515515b4d55ea588f40a89e7e3db2dad3d7
michelexe <enzo.fabre@orange.fr>
2018-04-25 12:21:29 +0200

    Initial commit

---
+++ client/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <div id="app">
+    <v-toolbar fixed tabs>
+      <v-tabs icons-and-text centered dark color="teal lighten-3" >
+        <v-tabs-slider color="red"></v-tabs-slider>
+        <v-tab href="#tab-1" color="yellow">
+          Recents
+          <v-icon>phone</v-icon>
+        </v-tab>
+        <v-tab href="#tab-2">
+          Contact
+          <v-icon>favorite</v-icon>
+        </v-tab>
+        <v-tab href="#tab-3">
+          Nearby
+          <v-icon>account_box</v-icon>
+        </v-tab>
+      </v-tabs>
+    </v-toolbar>
+    <v-container fluid>
+      <router-view/>
+    </v-container>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App'
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  height: 1080px;
+}
+#fixed_toolbar{
+  position:sticky;
+  top:1px;
+  z-index: 7;
+}
+@import '~vuetify/src/stylus/main.styl';
+</style>
---
+++ client/src/components/Carousel.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-carousel hide-controls dark hide-delimiters
+              transition="fade"
+              reverse-transition="fade"
+  >
+    <v-carousel-item v-for="(item,i) in items" :src="item.src" :key="i"></v-carousel-item>
+  </v-carousel>
+</template>
+
+<script>
+export default {
+  name: 'Carousel',
+  data () {
+    return {
+      items: [
+        {
+          src: '/static/carousel_image/5.jpg'
+        },
+        {
+          src: '/static/carousel_image/6.png'
+        },
+        {
+          src: '/static/carousel_image/3.jpg'
+        },
+        {
+          src: '/static/carousel_image/4.jpg'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
