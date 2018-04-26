7a8c3dc8219e381ffcef6745c67f085e907d4645
ophpkr <ophelie.piekarek@etu.umontpellier.fr>
2018-04-26 17:50:01 +0200

    presentation login

--- client/src/components/part/Login.vue
+++ client/src/components/part/Login.vue
@@ -1,67 +1,70 @@
 <template>
-  <div
-    id="e3"
-    style="max-width: 600px; margin: auto;">
-    <v-app>
-      <v-card>
-        <v-container grid-list-md text-xs-center>
+  <v-container align-start fluid>
+    <v-layout row wrap>
+      <v-flex xs12 md8 offset-md2 class="my-5">
+        <v-card>
+          <v-toolbar color="pink darken-1" dark>
+            <v-spacer></v-spacer>
+            <v-toolbar-title>Se connecter</v-toolbar-title>
+            <v-spacer></v-spacer>
+          </v-toolbar>
           <div v-if="estAssMat">
-            <form>
-              <v-text-field
-                :box="true"
-                label="Login"
-                v-model="logAssMat"
-                :error-messages="loginErrors"
-                @blur="$v.login.$touch()"
-                required
-              ></v-text-field>
-              <v-text-field
-                :box="true"
-                label="Mot de passe"
-                type="password"
-                v-model="mdpAM"
-                :error-messages="mdpErrors"
-                @blur="$v.mdpAM.$touch()"
-                required
-              ></v-text-field>
-              <br>
-              <div class="error" v-html="error" />
-              <br>
-              <v-btn round @click="loginAssMat" color="pink darken-1">Se connecter</v-btn>
-              <v-btn round @click="clearAssMat" color="pink darken-1">Retour</v-btn>
-            </form>
+            <v-card-text>
+              <v-form>
+                <v-text-field
+                  box="true"
+                  label="Login"
+                  v-model="logAssMat"
+                  :error-messages="loginErrors"
+                  @blur="$v.login.$touch()"
+                  required
+                ></v-text-field>
+                <v-text-field
+                  box
+                  label="Mot de passe"
+                  type="password"
+                  v-model="mdpAM"
+                  :error-messages="mdpErrors"
+                  @blur="$v.mdpAM.$touch()"
+                  required
+                ></v-text-field>
+                <div class="error" v-html="error" />
+                <v-btn round @click="loginAssMat" color="pink darken-1" class="white--text">Se connecter</v-btn>
+                <v-btn round @click="clearAssMat" color="pink darken-1" class="white--text">Retour</v-btn>
+              </v-form>
+            </v-card-text>
           </div>
           <div v-else>
-            <form>
-              <v-text-field
-                :box="true"
-                label="E-mail"
-                v-model="email"
-                :error-messages="emailErrors"
-                @blur="$v.email.$touch()"
-                required
-              ></v-text-field>
+            <v-card-text>
+              <v-form>
+                <v-text-field
+                  box="true"
+                  label="E-mail"
+                  v-model="email"
+                  :error-messages="emailErrors"
+                  @blur="$v.email.$touch()"
+                  required
+                ></v-text-field>
 
-              <v-text-field
-                :box="true"
-                label="Mot de passe"
-                type="password"
-                v-model="mdp"
-                :error-messages="mdpErrors"
-                @blur="$v.mdp.$touch()"
-                required
-              ></v-text-field>
-              <br>
-              <div class="error" v-html="error" />
-              <br>
-              <v-btn round @click="loginParent" color="pink darken-1">Se connecter</v-btn>
-              <v-btn round @click="clearParent" color="pink darken-1">Retour</v-btn>
-            </form>
+                <v-text-field
+                  box
+                  label="Mot de passe"
+                  type="password"
+                  v-model="mdp"
+                  :error-messages="mdpErrors"
+                  @blur="$v.mdp.$touch()"
+                  required
+                ></v-text-field>
+                <div class="error" v-html="error" />
+                <v-btn round @click="loginParent" color="pink darken-1" class="white--text">Se connecter</v-btn>
+                <v-btn round @click="clearParent" color="pink darken-1" class="white--text">Retour</v-btn>
+              </v-form>
+            </v-card-text>
           </div>
-        </v-container>
-      </v-card>
-    </v-app>
-  </div>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -122,11 +125,10 @@ export default {
       } catch (error) {
         this.error = error.response.data.error
       }
-
     },
     clearAssMat () {
       this.$v.$reset()
-      this.email= ''
+      this.email = ''
       this.mdp = ''
     },
     async loginAssMat () {
@@ -138,7 +140,6 @@ export default {
       } catch (error) {
         this.error = error.response.data.error
       }
-
     }
   }
 }
