b00976bda048aff3ef5f217a1af6426328abd076
ophpkr <ophelie.piekarek@etu.umontpellier.fr>
2018-05-01 18:54:04 +0200

    nouveaux composants contrat

---
+++ client/src/components/part/contratPart/DateDebutContrat.vue
@@ -0,0 +1,67 @@
+<template>
+  <v-layout row wrap>
+    <v-flex  xs12 md6 offset-md3 class="my-5">
+      <v-toolbar :color="toolbarColor" class="light-blue lighten-4">
+        <v-spacer></v-spacer>
+        <v-toolbar-title>Début du contrat</v-toolbar-title>
+        <v-spacer></v-spacer>
+      </v-toolbar>
+      <v-form>
+        <v-flex xs12 sm6 md4 mt-3 offset-md4>
+          <v-menu
+            ref="menu"
+            lazy
+            :close-on-content-click="false"
+            v-model="menu"
+            transition="scale-transition"
+            offset-y
+            full-width
+            :nudge-right="40"
+            min-width="290px"
+            :return-value.sync="date"
+          >
+            <v-text-field
+              slot="activator"
+              color="blue"
+              label="Date de fin de contrat"
+              v-model="date"
+              prepend-icon="event"
+              readonly
+            ></v-text-field>
+            <v-date-picker
+              locale="fr"
+              v-model="date"
+              no-title
+              scrollable
+              >
+              <v-spacer></v-spacer>
+            </v-date-picker>
+          </v-menu>
+        </v-flex>
+        <v-flex offset-md1>
+          <h3>Nombre de semaines de congés supplémentaires de l'employeur : </h3>
+          <v-flex offset-md4 md2>
+          <v-text-field
+            label="Nombre"
+            color="blue"
+          ></v-text-field>
+          </v-flex>
+        </v-flex>
+      </v-form>
+    </v-flex>
+  </v-layout>
+</template>
+
+<script>
+export default {
+  name: 'DateFinContrat',
+  data: () => ({
+    date: null,
+    menu: false
+  })
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ client/src/components/part/contratPart/PresenceEnfant.vue
@@ -0,0 +1,174 @@
+<template>
+  <v-card>
+    <v-layout row wrap>
+      <v-flex  xs12 md6 offset-md3 class="my-5">
+
+        <v-toolbar :color="toolbarColor" class="pink lighten-4">
+          <v-spacer></v-spacer>
+          <v-toolbar-title>Planning d'accueil</v-toolbar-title>
+          <v-spacer></v-spacer>
+        </v-toolbar>
+
+        <v-container>
+          <v-layout>
+
+            <v-flex md12 mt-3>
+              <v-card>
+                <v-list>
+                  <v-list-tile avatar v-for="item in items" :key="item.title" @click="afficheLundi">
+                    <v-list-tile-avatar>
+                      <v-icon color="pink lighten-4" :class="[item.iconClass]">{{ item.icon }}</v-icon>
+                    </v-list-tile-avatar>
+                    <v-list-tile-content>
+                      <v-list-tile-title>{{ item.title }}</v-list-tile-title>
+                      <v-list-tile-sub-title>{{ item.subtitle }}</v-list-tile-sub-title>
+                    </v-list-tile-content>
+                  </v-list-tile>
+                </v-list>
+              </v-card>
+            </v-flex>
+
+            <v-flex xs6 md12 mt-3>
+              <v-layout row>
+                <v-flex d-flex>
+
+                  <v-card>
+                    <h2 class="pink--text">Horaires</h2>
+
+                    <v-container fluid>
+                      <v-layout row wrap>
+                        <v-flex xs12 md5 sm6>
+                          <v-subheader class="black--text" v-text="'Heure d\'arrivée'"></v-subheader>
+                        </v-flex>
+                        <v-flex xs12 md3 sm6>
+                          <v-select
+                            :items="heures"
+                            :filter="customFilter"
+                            v-model="a1"
+                            item-text="heure"
+                            label="hh"
+                            single-line
+                            autocomplete
+                          ></v-select>
+                        </v-flex>
+                        <v-flex xs12 md3 offset-md1 sm6>
+                          <v-select
+                            :items="minutes"
+                            :filter="customFilter"
+                            v-model="a2"
+                            item-text="minute"
+                            label="mm"
+                            single-line
+                            autocomplete
+                          ></v-select>
+                        </v-flex>
+                      </v-layout>
+                    </v-container>
+
+                    <v-container fluid>
+                      <v-layout row wrap>
+                        <v-flex xs12 md5 sm6>
+                          <v-subheader class="black--text" v-text="'Heure de départ'"></v-subheader>
+                        </v-flex>
+                        <v-flex xs12 md3 sm6>
+                          <v-select
+                            :items="heures"
+                            :filter="customFilter"
+                            v-model="a1"
+                            item-text="heure"
+                            label="hh"
+                            single-line
+                            autocomplete
+                          ></v-select>
+                        </v-flex>
+                        <v-flex xs12 md3 offset-md1 sm6>
+                          <v-select
+                            :items="minutes"
+                            :filter="customFilter"
+                            v-model="a2"
+                            item-text="minute"
+                            label="mm"
+                            single-line
+                            autocomplete
+                          ></v-select>
+                        </v-flex>
+                      </v-layout>
+                    </v-container>
+
+                    <v-container fluid>
+                      <v-flex offset-md3>
+                        <v-checkbox
+                          label="Prend le goûter"
+                          v-model="ex4"
+                          color="pink"
+                          value="true"
+                          hide-details
+                        ></v-checkbox>
+                      </v-flex>
+                    </v-container>
+
+                    <v-btn align="center" class="pink--text text--lighten-4" color="pink">Ok</v-btn>
+
+                  </v-card>
+
+                </v-flex>
+              </v-layout>
+            </v-flex>
+
+          </v-layout>
+        </v-container>
+
+      </v-flex>
+    </v-layout>
+  </v-card>
+</template>
+
+<script>
+export default {
+  name: 'PlanningEnfant',
+  data () {
+    return {
+      ex4: ['Prend_goûter'],
+      items: [
+        { icon: 'schedule', iconClass: 'pink', title: 'Lundi' },
+        { icon: 'schedule', iconClass: 'pink', title: 'Mardi' },
+        { icon: 'schedule', iconClass: 'pink', title: 'Mercredi' },
+        { icon: 'schedule', iconClass: 'pink', title: 'Jeudi' },
+        { icon: 'schedule', iconClass: 'pink', title: 'Vendredi' }
+      ],
+      heures: [
+        { heure: '06' },
+        { heure: '07' },
+        { heure: '08' },
+        { heure: '09' },
+        { heure: '10' },
+        { heure: '11' },
+        { heure: '12' },
+        { heure: '13' },
+        { heure: '14' },
+        { heure: '15' },
+        { heure: '16' },
+        { heure: '17' },
+        { heure: '18' },
+        { heure: '19' },
+        { heure: '20' }
+      ],
+      minutes: [
+        { minute: '00' },
+        { minute: '30' }
+      ],
+      }
+    },
+
+  methods: {
+    afficheLundi () {
+
+    }
+  }
+
+}
+</script>
+
+<style scoped>
+
+</style>
