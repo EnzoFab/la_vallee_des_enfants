cb0c2f4251959e418e6ad601a4347bf3241b47b2
ophpkr <ophelie.piekarek@etu.umontpellier.fr>
2018-05-07 17:23:02 +0200

    ordre

---
+++ client/src/components/part/VisualisationContratPart/Fiche.vue
@@ -0,0 +1,129 @@
+<template>
+  <v-layout mt-5>
+    <v-flex>
+      <v-flex md10 offset-md1>
+        <v-flex class="text-md-left">
+          <h1 class="blue--text">Enfant</h1>
+        </v-flex>
+        <v-card>
+          <v-flex pt-2 offset-md1 class="text-sm-left">
+            <h2>Marion Rul</h2>
+          </v-flex>
+          <v-flex pl-5 class="text-sm-left">
+            <v-layout mt-1>
+              <h4 class="orange--text text--darken-1 mr-1"><v-icon class="orange--text text--darken-1">cake</v-icon> Anniversaire : </h4><span>8 avril 1996</span>
+            </v-layout>
+            <v-layout pb-2 mt-1>
+              <h4 class="orange--text text--darken-1 mr-1"><v-icon class="orange--text text--darken-1">face</v-icon> Sexe : </h4><span>Fille</span>
+            </v-layout>
+          </v-flex>
+        </v-card>
+      </v-flex>
+      <v-flex md10 offset-md1 mt-3>
+        <v-flex class="text-md-left">
+          <h1 class="blue--text">Contrat</h1>
+        </v-flex>
+        <v-card>
+          <v-flex pt-2 offset-md1 class="text-sm-left">
+            <h2>Contrat n° AFT5-98SEY </h2>
+          </v-flex>
+          <v-flex pl-5 class="text-sm-left">
+            <v-layout mt-1>
+              <h4 class="orange--text text--darken-1 mr-1"><v-icon class="orange--text text--darken-1">description</v-icon> Type de contrat : </h4><span>CDI</span>
+            </v-layout>
+            <v-layout mt-1>
+              <h4 class="orange--text text--darken-1 mr-1"><v-icon class="orange--text text--darken-1">payment</v-icon> Mode de paiement : </h4><span>Espèces</span>
+            </v-layout>
+            <v-layout mt-1>
+              <h4 class="orange--text text--darken-1 mr-1"><v-icon class="orange--text text--darken-1">event</v-icon> Date de début du contrat </h4><span>5 mai 2018</span>
+            </v-layout>
+            <v-layout pb-2 mt-1>
+              <h4 class="orange--text text--darken-1 mr-1"><v-icon class="orange--text text--darken-1">event</v-icon> Date de fin du contrat : </h4><span>Aucune</span>
+            </v-layout>
+            <v-layout pb-2 mt-1>
+              <v-flex offset-md2>
+                <v-btn color="blue--text">Clôturer le contrat</v-btn>
+              </v-flex>
+            </v-layout>
+          </v-flex>
+        </v-card>
+      </v-flex>
+    </v-flex>
+    <v-flex md6 offset-md1>
+      <v-flex md10>
+        <v-flex class="text-sm-left">
+          <h1 class="blue--text">Employeur</h1>
+        </v-flex>
+        <v-card>
+          <v-flex offset-md1 class="text-sm-left">
+            <h2>Enzo Fabre</h2>
+          </v-flex>
+          <v-flex pl-5 class="text-md-left">
+            <v-layout mt-1>
+              <h4 class="orange--text text--darken-1 mr-1"><v-icon class="orange--text text--darken-1">home</v-icon> Adresse : </h4><span>3 rue des coquelicots 17300 La Rochelle</span>
+            </v-layout>
+            <v-layout mt-1>
+              <h4 class="orange--text text--darken-1 mr-1"><v-icon class="orange--text text--darken-1">phone</v-icon> Téléphone : </h4><span> 05 46 23 45 87</span>
+            </v-layout>
+            <v-layout mt-1>
+              <h4 class="orange--text text--darken-1 mr-1"><v-icon class="orange--text text--darken-1">local_post_office</v-icon> Email : </h4><span> zozio.foot@but.com</span>
+            </v-layout>
+            <v-layout mt-1 pb-3>
+              <h4 class="orange--text text--darken-1 mr-1"><v-icon class="orange--text text--darken-1">contacts</v-icon> Identifiant : </h4>ZoFa</span>
+            </v-layout>
+          </v-flex>
+        </v-card>
+      </v-flex>
+      <v-flex>
+        <v-flex>
+          <v-flex class="text-md-left">
+            <h1 class="blue--text">Parents</h1>
+          </v-flex>
+          <v-card>
+            <v-flex pt-2 offset-md1 class="text-sm-left">
+              <h2>Enzo Fabre</h2>
+            </v-flex>
+            <v-flex pl-5 class="text-md-left">
+              <v-layout mt-1>
+                <h4 class="orange--text text--darken-1 mr-1"><v-icon class="orange--text text--darken-1">phone</v-icon> Téléphone : </h4><span>04 04 05 08 32</span>
+              </v-layout>
+              <v-layout mt-1>
+                <h4 class="orange--text text--darken-1 mr-1"><v-icon class="orange--text text--darken-1">work</v-icon> Profession : </h4><span> Footballeur professionnel</span>
+              </v-layout>
+              <v-layout pb-3 mt-1>
+                <h4 class="orange--text text--darken-1 mr-1"><v-icon class="orange--text text--darken-1">contact_phone</v-icon> Téléphone professionnel : </h4><span>Non renseigné</span>
+              </v-layout>
+            </v-flex>
+            <v-divider></v-divider>
+            <v-flex pt-2 offset-md1 class="text-sm-left">
+              <h2>Zinedine Zidane</h2>
+            </v-flex>
+            <v-flex pl-5 class="text-md-left">
+              <v-layout mt-1>
+                <h4 class="orange--text text--darken-1 mr-1"><v-icon class="orange--text text--darken-1">phone</v-icon> Téléphone : </h4><span>04 04 05 08 32</span>
+              </v-layout>
+              <v-layout mt-1>
+                <h4 class="orange--text text--darken-1 mr-1"><v-icon class="orange--text text--darken-1">work</v-icon> Profession : </h4><span> Footballeur professionnel</span>
+              </v-layout>
+              <v-layout pb-2 mt-1>
+                <h4 class="orange--text text--darken-1 mr-1"><v-icon class="orange--text text--darken-1">contact_phone</v-icon> Téléphone professionnel : </h4><span>Non renseigné</span>
+              </v-layout>
+            </v-flex>
+          </v-card>
+        </v-flex>
+    </v-flex>
+    </v-flex>
+  </v-layout>
+
+
+</template>
+
+<script>
+export default {
+  name: 'Fiche'
+}
+</script>
+
+<style scoped>
+
+</style>
