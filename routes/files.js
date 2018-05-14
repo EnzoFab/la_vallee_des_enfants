5c4137a3f0c3f4e90d06cd267ccc3afad429a990
michelexe <enzo.fabre@orange.fr>
2018-05-15 00:17:35 +0200

    deploiement v3

--- routes/files.js
+++ routes/files.js
@@ -5,23 +5,29 @@ const fs = require('fs');
 router.post('/image', function (req, res) {
     if( !req.files) {
         return res.status(400).send({erreur: 'Aucun fichier envoyé'});
-    }
-    fs.readdir('./public/images', (err, files) => {
-        // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
-        let img = req.files.image;
-        let pathImg = 'images/' + files.length + img.name  // on rajoute l'image le numero d'image qu'il y avait avant elle
+    }else {
+        fs.readdir('./public/images', (err, files) => {
+            // The name of the input field (i.e. "sampleFile") is used to retrieve the uploaded file
+            if (err) {
+                res.statut(500).send(err)
+            } else {
+                let img = req.files.image;
+                let pathImg = 'images/' + files.length + img.name  // on rajoute l'image le numero d'image qu'il y avait avant elle
 
-        // Use the mv() method to place the file somewhere on your server
-        img.mv('./public/'+pathImg, function(err) {
-            if (err)
-                return res.status(500).send(err);
+                // Use the mv() method to place the file somewhere on your server
+                img.mv('./public/'+pathImg, function(err) {
+                    if (err)
+                        return res.status(500).send(err);
 
-            res.send({
-                erreur: null,
-                image: 'static/'+pathImg
-            });
+                    res.send({
+                        erreur: null,
+                        image: 'static/'+pathImg
+                    });
+                });
+            }
         });
-    });
+    }
+
 
 
 })
