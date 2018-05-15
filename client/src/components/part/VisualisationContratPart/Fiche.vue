0dd1838d95f3e98e17237d70e0130d0ec0037456
marionrul <marion.mary@hotmail.fr>
2018-05-15 13:21:03 +0200

    Cliquer sur contrat et voir la fiche OK

--- client/src/components/part/VisualisationContratPart/Fiche.vue
+++ client/src/components/part/VisualisationContratPart/Fiche.vue
@@ -205,17 +205,15 @@ export default {
     }
   },
   mounted () {
-    console.log('sdfghj')
     this.initDonnees(this.numeroContrat)
     this.initParents(this.numeroContrat)
   },
   methods: {
     async initDonnees () {
       try {
-        console.log('heyyyyyyyyyyyyyyyyyyooooooooh')
         const response = await ContratService.donneesContrat(this.numeroContrat)
         console.log(response.data)
-        this.numeroContrat = this.$route.params.numC
+        this.numeroContrat = this.numeroContrat
         this.modeDePaiementContrat = response.data.modepaiements
         this.nomEnfant = response.data.nom_enfant
         this.prenomEnfant = response.data.prenom_enfant
